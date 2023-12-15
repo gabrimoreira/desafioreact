459566ce1c2b9b9a3dccc2e439c5397325df130b
gabrimoreira <110856732+gabrimoreira@users.noreply.github.com>
2023-12-14 22:15:57 -0300

    Criação TitleText: criei e coloquei sem props o título, texto, título e número

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../Components/Navbar"
 import Footer from "../Components/Footer"
 import Slider from "../Components/Slider"
+import TitleText from "../Components/TitleText"
 import "../Style/Main.css"
 
 
@@ -11,6 +12,7 @@ function Main(){
         <div className="main">
             <Navbar/>
             <Slider/>
+            <TitleText></TitleText>
             <Footer/>
         </div>
     )
