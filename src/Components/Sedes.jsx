d95b194ab3e04334b917a361bc63e419a7980339
gabrimoreira <110856732+gabrimoreira@users.noreply.github.com>
2023-12-15 21:58:54 -0300

    Criação Sedes: finalize a sessão de sedes

--- src/Components/Sedes.jsx
+++ src/Components/Sedes.jsx
@@ -1,26 +1,26 @@
 import {useEffect, useState} from 'react'
-import "../Style/Servicos.css"
-function Servicos(){
-    const [Servicos, setServicos] = useState([]);
+import "../Style/Sedes.css"
+function Sedes(){
+    const [Sedes, setSedes] = useState([]);
     useEffect(()=>{
-    fetch("http://localhost:5000/Servicos", {
+    fetch("http://localhost:5000/sedes", {
         method: "GET",
         headers:{
             'Content-Type': 'application/json',
         },
     }).then((resp) => resp.json()).then((data)=>{
-        setServicos(data)
+        setSedes(data)
     })
     .catch((erro)=> console.log(erro))
 }, [])
-console.log(Servicos)
+console.log(Sedes)
     function Card(props) {
         return (
           <div className="card">
             <img src={props.imagem} className="card-img" alt={props.imagem} />
             <div className="card-body">
               <h5 className="card-titulo">
-                Serviço: {props.nomeServico}</h5>
+                Sede: {props.Local}</h5>
               <p className="card-texto">EXPLICAÇÃO: {props.texto}</p>
             </div>
           </div>
@@ -36,12 +36,12 @@ console.log(Servicos)
             Serviços
             </h1>
             <div className="cards">
-                {Servicos.map((servico) => (
+                {Sedes.map((Sede) => (
                     <Card 
-                        key={servico.nomeServico}
-                        nomeServico={servico.nomeServico}
-                        texto={servico.texto}
-                        imagem={servico.imagem}
+                        key={Sede.Local}
+                        Local={Sede.Local}
+                        texto={Sede.texto}
+                        imagem={Sede.imagem}
                     />
                 ))}
             </div>
@@ -50,4 +50,4 @@ console.log(Servicos)
 
     )
 }
-export default Servicos
+export default Sedes
--- src/Pages/Contato.jsx
+++ src/Pages/Contato.jsx
@@ -1,14 +1,15 @@
 import Header from "../Components/Header"
 import Footer from "../Components/Footer"
 import Navbar from "../Components/Navbar"
-
+import headerContato from "../Images/headerContato.jpg"
+import Sedes from "../Components/Sedes"
 function Contato(){
 
     return(
         <>
             <Navbar></Navbar>
-            <Header></Header>
-            <h1>Contato</h1>
+            <Header imagem = {headerContato} titulo = {"  NOSSOS CONTATOS"}></Header>
+            <Sedes></Sedes>
             <Footer></Footer>
         </>
     )
